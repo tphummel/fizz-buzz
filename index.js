12d34d3da954bb7ab3be45c047fdd16f93898b77
Tom Hummel <tohu@hey.com>
2022-07-18 21:27:44 -0700

    put back my preferred solution

--- index.js
+++ index.js
@@ -4,14 +4,10 @@ function fizzBuzz (input) {
   const divBy3 = input % 3 === 0
   const divBy5 = input % 5 === 0
 
-  if (divBy3 || divBy5) {
-    let output = ''
-    if (divBy3) output += 'Fizz'
-    if (divBy5) output += 'Buzz'
-    return output
-  } else {
-    return input
-  }
+  if (divBy3 && divBy5) return 'FizzBuzz'
+  if (divBy3) return 'Fizz'
+  if (divBy5) return 'Buzz'
+  return input
 }
 
 module.exports = fizzBuzz
