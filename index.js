734e0236c11da212815df3bc56043777b8c5fb34
Tom Hummel <tohu@hey.com>
2022-07-18 21:12:39 -0700

    handle 1-20 sequence

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 'use strict'
 
 function fizzBuzz (input) {
-  if (input === 1) return 1
-  if (input === 3) return 'Fizz'
-  if (input === 5) return 'Buzz'
-  if (input === 15) return 'FizzBuzz'
-  return true
+  const divBy3 = input % 3 === 0
+  const divBy5 = input % 5 === 0
+
+  if (divBy3 && divBy5) return 'FizzBuzz'
+  if (divBy3) return 'Fizz'
+  if (divBy5) return 'Buzz'
+  return input
 }
 
 module.exports = fizzBuzz
--- test/index.js
+++ test/index.js
@@ -3,11 +3,6 @@
 const tap = require('tap')
 const lib = require('..')
 
-tap.test('first test', function (t) {
-  t.ok(lib())
-  t.end()
-})
-
 tap.test('1 -> 1', (t) => {
   t.equal(lib(1), 1)
   t.end()
@@ -25,5 +20,12 @@ tap.test('5 -> Buzz', (t) => {
 
 tap.test('15 -> FizzBuzz', (t) => {
   t.equal(lib(15), 'FizzBuzz')
+  t.end()
+})
+
+tap.test('1-20 Sequence', (t) => {
+  const expected = '1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz'
+  const actual = Array(20).fill().map((element, index) => lib(index + 1)).join(',')
+  t.equal(actual, expected)
   t.end()
 })
